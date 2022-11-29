6c8d1168fd9ac283f9792f51e1d3a23b09a86c29
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-11-29 12:36:09 -0300

    Implementing Hook

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import './Title.css';
 
 function Title (props: any) {
+  const [num, setNum] = useState(0);
     return(
-    <h1>Oi  {props.nome}!</h1>
+      <>
+    <h1>clique pra apresentar um numero {num} vezes {props.nome}!</h1>
+    <button onClick={() => setNum(num + 1)}>
+      clique aqui
+    </button>
+    </>
+
       );
+      
     }
     
     export {Title};
